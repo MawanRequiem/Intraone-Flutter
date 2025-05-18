f841e4b7c1811ee01796cd4f6daf76fdd002e0fd
RakaRdh <rakaherdika15@gmail.com>
2025-05-18 12:20:53 +0700

    Ubah baseUrl

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,6 +1,6 @@
 class AppConstants {
   // Gunakan 10.0.2.2 untuk Android emulator. Ubah sesuai environment.
-  static const String baseUrl = 'http://10.0.2.2:9999/api';
+  static const String baseUrl = 'https://enduring-aria-454904-d8.et.r.appspot.com/api';
 
   static const Map<String, String> jsonHeaders = {
     'Content-Type': 'application/json',
